fd67045d2b8bbafe34ddae11cd72a2d0711ebb27
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-05 11:31:07 -0400

    API: Tomar la data de un tour específico según la id en la URL

--- app.js
+++ app.js
@@ -22,7 +22,30 @@ app.get("/api/v1/tours", (req, res) => {
   })
 });
 
+//Tomar un tour según el parámetro id de la URL
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  
+  const getTour = tours.find(tour => {
+    return tour[id] === req.params.id * 1
+  });
 
+  if (!getTour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    })
+  };
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: getTour
+    }
+  });
+});
+
+//Crear un nuevo tour
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newId}, req.body);
@@ -39,6 +62,8 @@ app.post("/api/v1/tours", (req, res) => {
   });
 });
 
+
+// Listener
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
 });
