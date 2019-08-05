7753f06506588c34c14192f5129eaf6279ceabb0
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-05 11:48:53 -0400

    API: Editar la data de un tour específico según su ID

--- app.js
+++ app.js
@@ -62,6 +62,22 @@ app.post("/api/v1/tours", (req, res) => {
   });
 });
 
+//Editar la data de un tour según la ID especificada en la URL
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    })
+  };
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated Tour"
+    }
+  })
+})
 
 // Listener
 app.listen(port, () => {
