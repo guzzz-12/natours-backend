5919039b6ec4acb455b5090aaf1a29be8d150039
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-05 21:43:09 -0400

    Refactorización: Controllers de tour y user separados en archivos independientes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,40 +1,5 @@
 const express = require("express");
-
-//Operaciones con usuarios
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
-};
+const {getAllUsers, createUser, getUser, updateUser, deleteUser} = require("../controllers/userController");
 
 const router = express.Router();
 router.route("/")
