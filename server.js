e15d1b0c7814810aa7bc0f20549e5cd2dd045d5e
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-07 07:52:10 -0400

    Agregado mongoose para manejar la base de datos

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({path: "./config.env"});
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
+
+const db = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD)
+
+//Conectar mongoose con la base de datos remota
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then((connection) => {
+  console.log("Remote DB connection successful")
+})
+
+// //Conectar mongoose con la base de datos local
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// })
+// .then((connection) => {
+//   console.log("Local DB connection successful")
+// })
 
 // Listener
 app.listen(port, () => {
