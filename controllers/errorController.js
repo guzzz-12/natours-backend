e346e79ec6d274f47a43387f902ce5580f09dea0
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-31 08:35:50 -0400

    El error mostrado depende del entorno (development o production)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,33 @@
+const errorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack
+  });
+}
+
+const errorProd = (error, res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong"
+    })
+  }
+}
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message
-  });
+
+  if (process.env.NODE_ENV === "development") {
+    errorDev(error, res)
+  } else if (process.env.NODE_ENV === "production") {
+    errorProd(error, res)
+  }
 }
