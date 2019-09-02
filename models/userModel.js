74ec18eaed946629cac52a88b525a2b750ee74a7
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-02 08:33:25 -0400

    Validar confirmación de contraseña y encriptarla con bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,9 +26,26 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, "You must confirm your password"]
+    required: [true, "You must confirm your password"],
+    validate: {
+      //Sólo se valida al crear un nuevo user con save() y create()
+      validator: function(val) {
+        return val === this.password
+      },
+      message: "Passwords don't match"
+    }
   }
-})
+});
+
+//Encriptar la contraseña del user
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = null;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
