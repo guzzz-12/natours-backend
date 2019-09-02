78fcae7ed18f5b2d27e08d9859247480bcc958af
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-02 11:24:49 -0400

    Tomar la data de todos los usuarios en la base de datos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,24 @@
+const User = require("../models/userModel");
+const ErrorHandler = require("../utils/errorHandler");
+
 //Operaciones con usuarios
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined"
-  })
+exports.getAllUsers = async (req, res, next) => {
+  try {    
+    const users = await User.find();
+
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users: users
+      }
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error
+    })
+  }
 };
 
 exports.getUser = (req, res) => {
