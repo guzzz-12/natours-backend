48cfa2d44be095d37211cd6c5316fa879f9a574b
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-03 11:23:51 -0400

    Funcionalidad para enviar emails a los usuarios mediante nodemailer

---
+++ utils/emailSender.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+
+const emailSender = async (options) => {
+  //Crear un transporter
+  const transporter = nodemailer.createTransport({
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // auth: {
+    //   user: process.env.EMAIL_USERNAME,
+    //   pass: process.env.EMAIL_PASSWORD
+    // }
+    host: "smtp-mail.outlook.com",
+    secureConnection: false,
+    port: 587,
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+      user: process.env.HOTMAIL_USER,
+      pass: process.env.HOTMAIL_PASSWORD
+    }
+  });
+
+  console.log(options)
+
+  //Definir las opciones del email
+  const mailOptions = {
+    from: "Natours App",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html: 
+  }
+
+  //Enviar el email
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return console.log(err)
+    }
+    return console.log(info)
+  });
+}
+
+module.exports = emailSender;
