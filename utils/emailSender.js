c15ce3206ee827997f792be60a44404616a4fd43
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-05 07:59:36 -0400

    Modificado el remitente de correo electrónico de nodemailer

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -3,39 +3,31 @@ const nodemailer = require("nodemailer");
 const emailSender = async (options) => {
   //Crear un transporter
   const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    // auth: {
-    //   user: process.env.EMAIL_USERNAME,
-    //   pass: process.env.EMAIL_PASSWORD
-    // }
-    host: "smtp-mail.outlook.com",
-    secureConnection: false,
-    port: 587,
-    tls: {
-       ciphers:'SSLv3'
-    },
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.HOTMAIL_USER,
-      pass: process.env.HOTMAIL_PASSWORD
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASSWORD
     }
   });
 
   //Definir las opciones del email
   const mailOptions = {
-    from: "Natours App",
+    from: "Natours Website",
     to: options.email,
     subject: options.subject,
     text: options.message
   }
 
   //Enviar el email
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      return console.log("Error sending emal")
-    }
-    return console.log("Email sent successfully")
-  });
+  transporter.sendMail(mailOptions)
+  .then(info => {
+    console.log(info)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
 
 module.exports = emailSender;
