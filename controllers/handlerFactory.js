6a8a81e649008ee41141d679bffef2a43d372f70
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-07 07:28:35 -0400

    Centralizada funcionalidad para actualizar (Update) documentos
    La funcionalidad para editar documentos se centralizó en el archivo handlerFactory para evitar la repetición de código en los controllers

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,11 +1,32 @@
 const ErrorHandler = require("../utils/errorHandler");
 
+//Función para mostrar errores de validación
+const validationErrors = (err) => {
+  const errorProperties = Object.values(err.errors)
+  let allErrors = errorProperties.map((error) => {
+    return `${error.message}.`
+  })
+  const message = `Invalid data: ${allErrors.join(" ")}`
+  return new ErrorHandler(message, 400).message
+}
+
+//Función para mostrar errores de data duplicada
+const duplicateDataErrors = (err) => {
+  const value = err.errmsg.split(":")
+  const message = `Duplicate field: ${value[value.length - 1].replace(" \"", "").replace("\" }", "")}. Please try another name.`;
+  return new ErrorHandler(message, 400).message
+}
+
 //Función para mostrar errores de ID no válida
 const castErrors = (err) => {
   const newMessage = `Invalid ${err.path}: ${err.value}`
   return new ErrorHandler(newMessage, 404).message
 }
 
+
+//------Operaciones CRUD------//
+
+//Borrar un documento
 exports.deleteOne = (Model) => {
   return async (req, res, next) => {
     try {
@@ -30,6 +51,48 @@ exports.deleteOne = (Model) => {
         }
       }
   
+      res.status(404).json({
+        status: "fail",
+        message: err
+      })
+    }
+  }
+}
+
+//Actualizar un documento
+exports.updateOne = (Model) => {
+  return async (req, res, next) => {
+    try {
+      const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+      });
+  
+      if(!doc) {
+        return next(new ErrorHandler("No document found for that ID", 404))
+      }
+  
+      res.status(200).json({
+        status: "success",
+        data: {
+          data: doc
+        }
+      })
+    } catch (error) {
+      let err = {...error}
+      if (process.env.NODE_ENV === "production") {
+        if (err.name === "ValidationError") {
+          err = validationErrors(error)
+        }
+  
+        if(err.code === 11000) {
+          err = duplicateDataErrors(error)
+        }
+  
+        if(error.name === "CastError") {
+          err = castErrors(error)
+        }
+      }
       res.status(404).json({
         status: "fail",
         message: err
