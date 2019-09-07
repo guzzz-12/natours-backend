c3d81215014ccfd11d14b2bab9f6a048857ac0aa
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-07 13:53:21 -0400

    Corrección de autorizaciones para acceder a los routes de los usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,23 +4,26 @@ const {signup, login, forgotPassword, resetPassword, protectRoutes, restrictTo, 
 
 const router = express.Router();
 
+//Rutas que no requieren autenticación para acceder
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgotpassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
-router.patch("/updatePassword", protectRoutes, updatePassword);
 
+//Rutas que requieren autenticación de usuario para acceder
 router.get("/me", protectRoutes, getMe, getUser);
 router.patch("/updateMe", protectRoutes, updateMe);
 router.delete("/deleteMe", protectRoutes, deleteMe);
+router.patch("/updatePassword", protectRoutes, updatePassword);
 
+//Rutas que requieren autenticación de adminitrador para acceder
 router.route("/")
-.get(getAllUsers)
-.post(createUser);
+.get(protectRoutes, restrictTo("admin"), getAllUsers)
+.post(protectRoutes, restrictTo("admin"), createUser);
 
 router.route("/:id")
-.get(getUser)
-.patch(updateUser)
+.get(protectRoutes, restrictTo("admin"), getUser)
+.patch(protectRoutes, restrictTo("admin"), updateUser)
 .delete(protectRoutes, restrictTo("admin"), deleteUser);
 
 module.exports = router;
