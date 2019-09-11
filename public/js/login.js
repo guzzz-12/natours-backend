e39874b3fa11d460df81894c390b678ba884d8e4
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-11 09:13:09 -0400

    Formulario de login y funcionalidad para hacer login

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const login = (email, password) => {
+  axios({
+    method: "POST",
+    url: "http://localhost:3000/api/v1/users/login",
+    data: {
+      email: email,
+      password: password
+    }
+  })
+  .then(res => {
+    if (res.data.status === "success") {
+      alert("Logged in successfully");
+      window.setTimeout(() => {
+        location.assign("/")
+      }, 1500)
+    }
+  })
+  .catch(err => {
+    alert(err.response.data.message);
+  })
+}
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  login(email, password);
+});
