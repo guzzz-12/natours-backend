32e045e968e4da40e4873297d356c640a3324937
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-16 20:50:25 -0400

    Rutas para las operaciones CRUD de los bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const {getCheckoutSession} = require("../controllers/bookingController");
-const {protectRoutes} = require("../controllers/authController")
+const {getCheckoutSession, getAllBookings, createBooking, getBooking, updateBooking, deleteBooking} = require("../controllers/bookingController");
+const {protectRoutes, restrictTo} = require("../controllers/authController")
 
 const router = express.Router();
 
 router.get("/checkout-session/:tourId", protectRoutes, getCheckoutSession);
+
+router.route("/")
+.get(protectRoutes, restrictTo("admin", "lead-guide"), getAllBookings)
+.post(protectRoutes, restrictTo("admin", "lead-guide"), createBooking)
+
+router.route("/:id")
+.get(protectRoutes, restrictTo("admin", "lead-guide"), getBooking)
+.patch(protectRoutes, restrictTo("admin", "lead-guide"), updateBooking)
+.delete(protectRoutes, restrictTo("admin", "lead-guide"), deleteBooking)
 
 module.exports = router;
